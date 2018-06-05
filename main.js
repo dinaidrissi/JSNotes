2a8301013299bcf59e1efc752e4002b482098136
Dina Idrissi <dinaidrissi@desktop-77hir8b.kea.local>
2018-06-05 10:53:28 +0200

    deleting works

--- main.js
+++ main.js
@@ -24,13 +24,25 @@ form.addEventListener("submit", (e)=>{
 database.ref("notes/").on("child_added", (snapshot)=>{
     const key = snapshot.key;
     const data = snapshot.val();
-console.log(snapshot);
+//console.log(snapshot);
 const clone = template.cloneNode(true);
+clone.querySelector("article").dataset.key=key;
 clone.querySelector("h1").textContent = data.header;
 clone.querySelector("div.description").textContent = data.description;
+clone.querySelector("button.delete").addEventListener("click", e=>{
+database.ref("notes/" + key).remove();
+});
 app.appendChild(clone);
 });
 
+//listen for removal of data child_removed
+
+database.ref("notes/").on("child_removed", snapshot=>{
+    const key = snapshot.key;
+    let el = document.querySelector(`article[data-key=${key}]`);
+    el.remove();
+    //console.log(el);
+})
 
 
 
